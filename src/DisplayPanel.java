238e1478794beafcd977e1a221ac21e522ab93d5
BT_1N3_07 <sample@gmail.com>
2025-05-21 13:37:06 -0400

    initial

---
+++ src/DisplayPanel.java
@@ -0,0 +1,25 @@
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DisplayPanel extends JPanel implements ActionListener {
+
+    public DisplayPanel() {
+
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
